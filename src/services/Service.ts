787389028d71527db39f5ac4dc9c0e2c8496220e
Sérgio Luiz <100054963+SergioLNeves@users.noreply.github.com>
2023-01-29 19:23:46 -0300

    Update Service.ts

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://alimentacao.onrender.com'
+    baseURL: 'https://alimentacao.onrender.com/'
 })
 
 export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
